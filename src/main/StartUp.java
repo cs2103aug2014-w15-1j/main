41d1d751268a2eecbd4581d8a1034e14acdb4750
sundychenD <sundychenya1018@yahoo.com>
2014-09-23 14:08:01 +0800

    testing commit

--- src/main/StartUp.java
+++ src/main/StartUp.java
@@ -6,6 +6,7 @@ public class StartUp {
 		System.out.println("Hello world");
 		System.out.println("Testing Change file");
 		System.out.println("Testing Change file 2");
+		System.out.println("Testing Change file 3");
 
 	}
 
