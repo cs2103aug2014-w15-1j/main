9a9e1b0a26716aa53c4aaae3d9cb5002ddddcba5
sundychenD <sundychenya1018@yahoo.com>
2014-09-27 01:17:26 +0800

    storing data class prototype already

--- src/data_store/StoringData.java
+++ src/data_store/StoringData.java
@@ -1,8 +1,66 @@
 package data_store;
 
-public class StoringData {
-	public void main(String[] args){
-		System.out.println("Testing New Package");
-	}
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
+/* This class stores data to file by passing (String)filename
+ * Initialization with parameter (ArrayList<String>) data is required
+ * */
+
+class StoringData {
+	
+	private ArrayList<String> TRASHDATA;
+	private ArrayList<String> EVENTDATA;
+	private String TRASHERROR = " Trash file writing error ";
+	private String EVENTERROR = " Event file writing error ";
+	
+	public StoringData() {
+		this.TRASHDATA = new ArrayList<String>();
+		this.EVENTDATA = new ArrayList<String>();
+	}
+	
+	public StoringData(ArrayList<String> trash, ArrayList<String> event) {
+		this.TRASHDATA = trash;
+		this.EVENTDATA = event;
+	}
+	
+	public void writeAllData(String TrashFileName, String EventFileName) {
+		writeTrash(TrashFileName);
+		writeEvent(EventFileName);
+	}
+	
+	public void writeTrash(String TrashFileName) {
+		try {
+			FileWriter fw = new FileWriter (TrashFileName);
+			BufferedWriter bw = new BufferedWriter (fw);
+			PrintWriter fileOut = new PrintWriter (bw);
+			writeLineAL(this.TRASHDATA, fileOut);
+			fileOut.close();
+		}
+		catch (Exception e) {
+			System.out.println(TRASHERROR + e.toString());
+		}
+	}
+	
+	public void writeEvent(String EventFileName) {
+		try {
+			FileWriter fw = new FileWriter (EventFileName);
+			BufferedWriter bw = new BufferedWriter (fw);
+			PrintWriter fileOut = new PrintWriter (bw);
+			writeLineAL(this.EVENTDATA, fileOut);
+			fileOut.close();
+		}
+		catch (Exception e) {
+			System.out.println(EVENTERROR + e.toString());
+		}
+	}
+	
+	protected void writeLineAL(ArrayList<String> Data, PrintWriter fileOut) {
+		for (int i = 0; i < Data.size(); i++) {
+			fileOut.println(Data.get(i)); 
+		}
+	}
+	
 }
