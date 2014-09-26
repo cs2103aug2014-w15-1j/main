561ac039c1b24771b5710da5eb78e40870fccb35
sundychenD <sundychenya1018@yahoo.com>
2014-09-27 01:28:33 +0800

    comments added

--- src/data_store/StoringData.java
+++ src/data_store/StoringData.java
@@ -5,7 +5,8 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-/* This class stores data to file by passing (String)filename
+/**
+ * This class stores data to file by passing (String)filename
  * Initialization with parameter (ArrayList<String>) data is required
  * */
 
@@ -16,6 +17,9 @@ class StoringData {
 	private String TRASHERROR = " Trash file writing error ";
 	private String EVENTERROR = " Event file writing error ";
 	
+	/**
+	 * Constructor
+	 * */
 	public StoringData() {
 		this.TRASHDATA = new ArrayList<String>();
 		this.EVENTDATA = new ArrayList<String>();
@@ -26,11 +30,29 @@ class StoringData {
 		this.EVENTDATA = event;
 	}
 	
+	/**
+	 * Methods
+	 * */
+	
+	/**
+	 * Writing all data to distinctive file
+	 * 
+	 * @param TrashFileName
+	 *            is the (String) name of the trash file
+	 * @param EventFileName
+	 *            is the (String) name of the event file
+	 */
 	public void writeAllData(String TrashFileName, String EventFileName) {
 		writeTrash(TrashFileName);
 		writeEvent(EventFileName);
 	}
 	
+	/**
+	 * Only write to trash file
+	 * 
+	 * @param TrashFileName
+	 *            is the (String) name of the trash file
+	 */
 	public void writeTrash(String TrashFileName) {
 		try {
 			FileWriter fw = new FileWriter (TrashFileName);
@@ -44,6 +66,12 @@ class StoringData {
 		}
 	}
 	
+	/**
+	 * Only write to event file
+	 * 
+	 * @param EventFileName
+	 *            is the (String) name of the event file
+	 */
 	public void writeEvent(String EventFileName) {
 		try {
 			FileWriter fw = new FileWriter (EventFileName);
@@ -57,6 +85,14 @@ class StoringData {
 		}
 	}
 	
+	/**
+	 * Write to file line by line
+	 * 
+	 * @param Data
+	 *            is the (ArrayList<String>) that stores data
+	 * @param fileOut
+	 *            (PrintWriter) of the file
+	 */
 	protected void writeLineAL(ArrayList<String> Data, PrintWriter fileOut) {
 		for (int i = 0; i < Data.size(); i++) {
 			fileOut.println(Data.get(i)); 
