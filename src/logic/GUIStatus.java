04116731528f1d88f6ce9de10f06fed38fa96544
Ziyu <david_ziyu@outlook.com>
2014-09-29 05:37:52 +0800

    zy.0.5

--- src/logic/GUIStatus.java
+++ src/logic/GUIStatus.java
@@ -4,17 +4,20 @@ public class GUIStatus {
 	private int ViewMode;
 	private boolean hasNext;
 	private boolean hasPrevious;
+	private int currentTask;
 	
 	GUIStatus(){
 		this.ViewMode = 0;
 		this.hasNext = false;
 		this.hasPrevious = false;
+		this.currentTask = 0;
 	}
 	
-	GUIStatus(int ViewMode, boolean hasNext, boolean hasPrevious){
+	GUIStatus(int ViewMode, boolean hasNext, boolean hasPrevious, int curretnTask){
 		this.ViewMode = ViewMode;
 		this.hasNext = hasNext;
 		this.hasPrevious = hasPrevious;
+		this.currentTask = curretnTask;
 	}
 	
 	public int getMode(){
@@ -27,5 +30,24 @@ public class GUIStatus {
 	
 	public boolean hasPrevious(){
 		return this.hasPrevious;
+	}
+	
+	public int getTask(){
+		return this.currentTask;
+	}
+	public void changeViewMode(int mode){
+		this.ViewMode = mode;
+	}
+	
+	public void changeHasNext(){
+		this.hasNext = !this.hasNext;
+	}
+	
+	public void changeHasPrevious(){
+		this.hasPrevious = !this.hasPrevious;
+	}
+	
+	public void changeCurretnTask(int task){
+		this.currentTask = task;
 	}
 }
--- src/logic/LogicToGui.java
+++ src/logic/LogicToGui.java
@@ -7,19 +7,22 @@ public class LogicToGui {
 	private ArrayList<Task> TaskList;
 	private String feedback;
 	private String title;
+	private int currentTask;
 	
 	LogicToGui(){
 		this.GUI = new GUIStatus();
 		this.TaskList = null;
-		feedback = null;
-		title = null;
+		this.feedback = null;
+		this.title = null;
+		this.currentTask = -1;
 	}
 	
-	LogicToGui(GUIStatus GUI, ArrayList<Task> taskList, String feedback, String title){
+	LogicToGui(GUIStatus GUI, ArrayList<Task> taskList, String feedback, String title, int currentTask){
 		this.GUI = GUI;
 		this.TaskList = taskList;
 		this.feedback = feedback;
 		this.title = title;
+		this.currentTask = currentTask;
 	}
 	
 	public GUIStatus getGUIstatus(){
@@ -36,5 +39,9 @@ public class LogicToGui {
 	
 	public String getTitle(){
 		return this.title;
+	}
+	
+	public int getCurrentTask(){
+		return this.currentTask;
 	}
 }
