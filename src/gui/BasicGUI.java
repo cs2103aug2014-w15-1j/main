5d130fdc100c5de087037bc10f122a8a6962a1bc
Zhang Ji <zhangji951027@gmail.com>
2014-09-30 17:06:40 +0800

    refactor for basic GUI and displayConfiguration v0.1

--- src/gui/BasicGUI.java
+++ src/gui/BasicGUI.java
@@ -23,7 +23,6 @@ import javax.swing.SwingConstants;
 
 import java.awt.Font;
 
-
 import java.awt.Dimension;
 
 import logic.RunLogic;
@@ -32,30 +31,50 @@ import javax.swing.JToolBar;
 import javax.swing.JLayeredPane;
 
 /**
- * class BasicGUI: contains the basic structure of GUI 
- * @author JJ
- * GUI is mainly constructs by 4 container: 
- * titlePanel(for title), desktopPanel(for main), toolBar(for helper) and inputPanel(for input)
- * And inside each container, there exist a hierarchy of container and field.
+ * class BasicGUI: contains the basic structure of GUI
+ * 
+ * @author JJ GUI is mainly constructs by 4 container: titlePanel(for title),
+ *         desktopPanel(for main), toolBar(for helper) and inputPanel(for input)
+ *         And inside each container, there exist a hierarchy of container and
+ *         field.
  */
 public class BasicGUI extends JFrame {
+	
+	/*
+	 * ====================================================================
+	 * ===================== START OF PRIVATE FIELD =======================
+	 * ====================================================================
+	 */
+	
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private JPanel titlePanel;
 	private TextField titleWindow;
-	
+
 	private JToolBar toolBar;
 	private JTextArea HelpWindow;
-	
+
 	private JDesktopPane desktopPanel;
 	private JPanel FeedbackPanel;
 	private JTextField feedbackWindow;
 	private JLayeredPane layeredPaneForMain;
 	private TextArea mainWindow;
-	
+
 	private JPanel inputPanel;
 	private JTextField inputWindow;
+
+	// constants for FRAME initialization (unit in pixel)
+	private final static int TOP_LEFT_X_VALUE = 100;
+	private final static int TOP_LEFT_Y_VALUE = 100;
+	private final static int FRAME_WIDTH = 100;
+	private final static int FRAME_HEIGHT = 100;
+	
+	/*
+	 * ====================================================================
+	 * ===================== END OF PRIVATE FIELD =========================
+	 * ====================================================================
+	 */
 	
 	/**
 	 * Launch the application.
@@ -72,27 +91,98 @@ public class BasicGUI extends JFrame {
 			}
 		});
 	}
-
+	/*
+	 * ====================================================================
+	 * ===================== START OF PUBLIC METHOD =======================
+	 * ====================================================================
+	 */
+	
 	/**
-	 * Create the frame.
+	 * method BasicGUI: constructor of GUI 
 	 */
 	public BasicGUI() {
-		getContentPane().setEnabled(false);
-		getContentPane().setForeground(Color.WHITE);
-		getContentPane().setBackground(Color.WHITE);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 307);
-		
+		constructContentPane();
+		constructTitlePane();
+		constructBodyPane();
+		constructHelperPane();
+		constructInputPane();
+	}
+	
+	public void setTitleText(String text) {
+		titleWindow.setText(text);
+	}
+
+	public void setFeedbackText(String text) {
+		feedbackWindow.setText(text);
+	}
+
+	public void setMainText(String text) {
+		mainWindow.setText(text);
+	}
+
+
+	/*
+	 * ====================================================================
+	 * ===================== END OF PUBLIC METHOD =========================
+	 * ====================================================================
+	 */
+	private void constructInputPane() {
+		inputPanel = new JPanel();
+		getContentPane().add(inputPanel, BorderLayout.SOUTH);
+		inputPanel.setLayout(new BorderLayout(0, 0));
+
+		inputWindow = new JTextField();
+		inputPanel.add(inputWindow);
+		inputWindow.setBackground(Color.WHITE);
+		inputWindow.addActionListener(new inputHit());
+		inputWindow.setText("Input");
+		inputWindow.setColumns(30);
+	}
+
+	private void constructHelperPane() {
+		toolBar = new JToolBar();
+		toolBar.setFloatable(false);
+		toolBar.setSize(new Dimension(2314, 0));
+		toolBar.setRollover(true);
+		getContentPane().add(toolBar, BorderLayout.EAST);
+
+		HelpWindow = new JTextArea();
+		HelpWindow.setTabSize(1);
+		HelpWindow.setRows(2);
+		HelpWindow.setWrapStyleWord(true);
+		toolBar.add(HelpWindow);
+		HelpWindow.setBorder(null);
+		HelpWindow.setEditable(false);
+		HelpWindow.setBackground(new Color(0, 255, 204));
+		HelpWindow.setText("command");
+	}
+
+	private void constructTitlePane() {
+		titlePanel = new JPanel();
+		getContentPane().add(titlePanel, BorderLayout.NORTH);
+		titlePanel.setLayout(new BorderLayout(0, 0));
+
+		titleWindow = new TextField();
+		titlePanel.add(titleWindow);
+		titleWindow.setEditable(false);
+		titleWindow
+				.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		titleWindow.setText("Today is Sep 29 2014");
+		titleWindow.setForeground(Color.WHITE);
+		titleWindow.setBackground(Color.PINK);
+	}
+
+	private void constructBodyPane() {
 		desktopPanel = new JDesktopPane();
 		getContentPane().add(desktopPanel, BorderLayout.CENTER);
 		desktopPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		FeedbackPanel = new JPanel();
 		desktopPanel.add(FeedbackPanel, BorderLayout.SOUTH);
 		FeedbackPanel.setBackground(new Color(255, 255, 255));
 		FeedbackPanel.setBorder(null);
 		FeedbackPanel.setLayout(new BorderLayout(0, 0));
-		
+
 		feedbackWindow = new JTextField();
 		FeedbackPanel.add(feedbackWindow, BorderLayout.NORTH);
 		feedbackWindow.setMinimumSize(new Dimension(10, 8));
@@ -103,10 +193,10 @@ public class BasicGUI extends JFrame {
 		feedbackWindow.setEditable(false);
 		feedbackWindow.setText("Task Added!");
 		feedbackWindow.setColumns(20);
-		
+
 		layeredPaneForMain = new JLayeredPane();
 		desktopPanel.add(layeredPaneForMain, BorderLayout.NORTH);
-		
+
 		mainWindow = new TextArea();
 		mainWindow.setBounds(0, 0, 380, 215);
 		layeredPaneForMain.add(mainWindow);
@@ -119,50 +209,19 @@ public class BasicGUI extends JFrame {
 		mainWindow.setForeground(new Color(0, 0, 0));
 		mainWindow.setText("Do some thing!\nDo some thing more interesting!");
 		mainWindow.setBackground(Color.WHITE);
-		
-		toolBar = new JToolBar();
-		toolBar.setFloatable(false);
-		toolBar.setSize(new Dimension(2314, 0));
-		toolBar.setRollover(true);
-		getContentPane().add(toolBar, BorderLayout.EAST);
-		
-		HelpWindow = new JTextArea();
-		HelpWindow.setTabSize(1);
-		HelpWindow.setRows(2);
-		HelpWindow.setWrapStyleWord(true);
-		toolBar.add(HelpWindow);
-		HelpWindow.setBorder(null);
-		HelpWindow.setEditable(false);
-		HelpWindow.setBackground(new Color(0, 255, 204));
-		HelpWindow.setText("command");
-		
-		titlePanel = new JPanel();
-		getContentPane().add(titlePanel, BorderLayout.NORTH);
-		titlePanel.setLayout(new BorderLayout(0, 0));
-		
-		titleWindow = new TextField();
-		titlePanel.add(titleWindow);
-		titleWindow.setEditable(false);
-		titleWindow.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-		titleWindow.setText("Today is Sep 29 2014");
-		titleWindow.setForeground(Color.WHITE);
-		titleWindow.setBackground(Color.PINK);
-		
-		inputPanel = new JPanel();
-		getContentPane().add(inputPanel, BorderLayout.SOUTH);
-		inputPanel.setLayout(new BorderLayout(0, 0));
-		
-		inputWindow = new JTextField();
-		inputPanel.add(inputWindow);
-		inputWindow.setBackground(Color.WHITE);
-		inputWindow.addActionListener(new inputHit());
-		inputWindow.setText("Input");
-		inputWindow.setColumns(30);
 	}
-	
+
+	private void constructContentPane() {
+		getContentPane().setEnabled(false);
+		getContentPane().setForeground(Color.WHITE);
+		getContentPane().setBackground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(TOP_LEFT_X_VALUE, TOP_LEFT_Y_VALUE, FRAME_WIDTH, FRAME_HEIGHT);
+	}
+
 	/**
-	 * method inputHit: a action listener for the enter key hit in input
-	 * if the user hit the enter key, the input message will be send to logic
+	 * method inputHit: a action listener for the enter key hit in input if the
+	 * user hit the enter key, the input message will be send to logic
 	 */
 	class inputHit implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
@@ -170,17 +229,9 @@ public class BasicGUI extends JFrame {
 			String emptyString = "";
 			inputWindow.setText(emptyString);
 			RunLogic.Logic(command);
-			
+
 		}
 	}
-	
-	public void setTitleText(String text){
-		titleWindow.setText(text); 
-	}
-	public void setFeedbackText(String text){
-		feedbackWindow.setText(text); 
-	}
-	public void setMainText(String text){
-		mainWindow.setText(text);
-	}
+
+
 }
