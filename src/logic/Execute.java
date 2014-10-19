5ec95b85a8bb6470be8ea199b604f8f295380f21
Zhang Ji <zhangji951027@gmail.com>
2014-10-19 21:34:33 +0800

    add multilayered gui for GUI VIEW

--- src/logic/Execute.java
+++ src/logic/Execute.java
@@ -20,7 +20,6 @@ public class Execute {
 	public static DisplayInfo executeCommand(Command userCommand){
 		initialize();
 		COMMAND_TYPE commandType = determineCommandType(userCommand.getCommand());
-		
 		switch (commandType) {
 			case ADD_TASK:
 				addTask(userCommand);
