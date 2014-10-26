9c8281bf16a5d87369aba2566a3546a0abb0d798
sundychen <sundychen@174-195.priv19.nus.edu.sg>
2014-10-27 00:54:48 +0800

    New NLP library added

---
+++ src/parser/Tokenizer.java
@@ -0,0 +1,42 @@
+package parser;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+
+
+public class Tokenizer {
+	
+	public static void Tokenize() throws IOException {
+		InputStream modelIn = new FileInputStream("lib/en-token.bin");
+
+		try {
+		  TokenizerModel model = new TokenizerModel(modelIn);
+		  TokenizerME tokenizer = new TokenizerME(model);
+		  String tokens[] = tokenizer.tokenize("An input sample sentence.");
+		  
+		  for (String word: tokens) {
+			  System.out.println(word);
+		  }
+		}
+		catch (IOException e) {
+		  e.printStackTrace();
+		}
+		finally {
+		  if (modelIn != null) {
+		    try {
+		      modelIn.close();
+		    }
+		    catch (IOException e) {
+		    }
+		  }
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		Tokenize();
+	}
+}
