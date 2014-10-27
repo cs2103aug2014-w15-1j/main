e4623131fe51f205d9b409439d669f29f1b89d7a
Zhang Ji <zhangji951027@gmail.com>
2014-10-28 03:22:06 +0800

    remove helper panel in GUI

--- src/gui/MemoListPanel.java
+++ src/gui/MemoListPanel.java
@@ -3,7 +3,10 @@ package gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Insets;
 import java.awt.Point;
 
 import javax.swing.BorderFactory;
@@ -16,8 +19,11 @@ import javax.swing.SwingConstants;
 
 public class MemoListPanel extends JPanel {
 
-	private String[] layerStrings = { "bin (0)", "done (1)", "today (2)",
+	private String[] title = { "bin (0)", "done (1)sdfafasfdsdfasfdasdfasdfasdfad", "today (2)",
 			"tomorrow (3)", "tasklist (4)" };
+	private String[] date = { "120130318", "221100408", "320100308",
+			"420100328", "520100308" };
+
 	private int alphaValue = 240;
 	private Color[] layerColors = { new Color(251, 172, 27, alphaValue),
 			new Color(52, 167, 224, alphaValue),
@@ -27,28 +33,42 @@ public class MemoListPanel extends JPanel {
 
 	MemoListPanel() {
 		super();
-		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		for (int i = 0; i < layerStrings.length; i++) {
-			if(i%2 == 0) {
-				System.out.println(i);
-				JLabel label = createColoredLabel(layerStrings[i], layerColors[i],
-						0);
-				this.add(label);
-			} else {
-				JLabel label = createColoredLabel(layerStrings[i], layerColors[i],
-						1);
-				this.add(label);
-			}
-			this.add(Box.createVerticalGlue());
+		setLayout(new GridBagLayout());
+		GridBagConstraints c1 = new GridBagConstraints();
+		c1.anchor = GridBagConstraints.FIRST_LINE_START;
+		c1.weightx = 0.5;
+		c1.fill = GridBagConstraints.BOTH;
+		c1.ipady = 10;  
+		c1.gridx = 0;
+		c1.gridy = 0;
+		c1.insets = new Insets(2,10,1,1);
+		c1.weightx = 0.5;
+		for(int i=0; i<title.length; i++) {
+			c1.gridy = i;
+			this.add(createColoredLabel(title[i], layerColors[i]), c1);
 		}
+		
+		GridBagConstraints c2 = new GridBagConstraints();
+		c2.gridx = 1;
+		c2.weightx = 0.2;
+		c2.insets = new Insets(2,10,1,1);
+		c2.fill = GridBagConstraints.BOTH;
+		for(int i=0; i<date.length; i++) {
+			c2.gridy = i;
+			this.add(createColoredLabel(date[i], layerColors[i]), c2);
+		}
+		
+	}
+	
+	public void setDisplayInfo() {
+		
 	}
 
 	// Create and set up a colored label.
-	private JLabel createColoredLabel(String text, Color color, float d) {
+	private JLabel createColoredLabel(String text, Color color) {
 		JLabel label = new JLabel(text);
 		label.setFont(label.getFont().deriveFont(Font.BOLD | Font.ITALIC));
-		//label.setVerticalAlignment(JLabel.TOP);
-		label.setAlignmentX(d);
+		// label.setVerticalAlignment(JLabel.TOP);
 		label.setOpaque(true);
 		label.setBackground(color);
 		label.setForeground(Color.black);
