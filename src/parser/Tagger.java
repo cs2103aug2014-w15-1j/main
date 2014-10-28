d1c2003de46c6877cc49a32bc4d7058e2369f17c
sundychen <sundychenya1018@yahoo.com>
2014-10-28 11:43:19 +0800

    Update Debugged

---
+++ src/parser/Tagger.java
@@ -0,0 +1,48 @@
+package parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+
+import opennlp.tools.cmdline.PerformanceMonitor;
+import opennlp.tools.cmdline.postag.POSModelLoader;
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSSample;
+import opennlp.tools.postag.POSTaggerME;
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+import opennlp.tools.util.ObjectStream;
+import opennlp.tools.util.PlainTextByLineStream;
+
+public class Tagger {
+	public static void POSTag() throws IOException {
+		POSModel model = new POSModelLoader()	
+			.load(new File("lib/en-pos-maxent.bin"));
+		PerformanceMonitor perfMon = new PerformanceMonitor(System.err, "sent");
+		POSTaggerME tagger = new POSTaggerME(model);
+	 
+		String input = "Add a new task tomorrow, buy gifts for Chirstina, start 2012-10-31, end 2012-12-20";
+		ObjectStream<String> lineStream = new PlainTextByLineStream(
+				new StringReader(input));
+	 
+		perfMon.start();
+		String line;
+		while ((line = lineStream.read()) != null) {
+	 
+			String whitespaceTokenizerLine[] = WhitespaceTokenizer.INSTANCE
+					.tokenize(line);
+			String[] tags = tagger.tag(whitespaceTokenizerLine);
+	 
+			POSSample sample = new POSSample(whitespaceTokenizerLine, tags);
+			System.out.println(sample.toString());
+	 
+			perfMon.incrementCounter();
+		}
+		perfMon.stopAndPrintFinalResult();
+	}
+	
+	/*
+	public static void main(String[] args) throws IOException {
+		POSTag();
+	}
+	*/
+}
