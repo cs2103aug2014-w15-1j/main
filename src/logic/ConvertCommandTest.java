fafa9cdfab8130d60c6e12fb21589b04086adbdb
Ziyu <david_ziyu@outlook.com>
2014-11-10 19:36:58 +0800

    zy.test

--- src/logic/ConvertCommandTest.java
+++ src/logic/ConvertCommandTest.java
@@ -16,26 +16,42 @@ import parser.RawCommand;
 public class ConvertCommandTest {
 	RawCommand RawCommand1;
 	RawCommand RawCommand2;
-	RawCommand RawCommand3;
-	Command expectedResult1;
-	Command expectedResult2;
-	Command expectedResult3;
+	Task task;
+	int[] display = new int[Default.MAX_DISPLAY_LINE + 1];
+	private static boolean arrayEquals(int[] a,int[] b) {
+		if(a == b) return true;
+		if(a.length != b.length) return false;
+		for(int i = 0; i<a.length; i++){
+			if(a[i] != b[i]) return false;			
+		}
+		return true;
+	}
 	
 	@Before
 	public void setUp() throws Exception {
-		RawCommand2 = new RawCommand("view", "trashbin");
-		RawCommand3 = new RawCommand("lllll");
-		expectedResult2 = new ViewTrashBin(0, "View mode changed!", "Trash bin");
-		expectedResult3 = new Invalid("Invalid Command!");
+		RunLogic.initialize();
+		RawCommand1 = new RawCommand("add", "title", null, null, "description");
+		task = new Task("title", "description", null, null);
+		RawCommand2 = new RawCommand("read", "1");
+		for(int i = 0; i < display.length; i++){
+			display[i] = -1;
+		}
 	}
 
 	@Test
 	public void test() {
-		Command result2 = ConvertCommand.convert(RawCommand2);
-		assertEquals(expectedResult1, result2);
+		Command result1 = ConvertCommand.convert(RawCommand1);
+		result1.execute();
+		assertEquals(true, RunLogic.getTaskList().get(RunLogic.getTaskList().size() - 1).equalIgnorePointer(task));
 		
-		Command result3 = ConvertCommand.convert(RawCommand3);
-		assertEquals(expectedResult1, result3);
+		Command help = new ViewTaskList(null, null);
+		help.execute();
+		Command result2 = ConvertCommand.convert(RawCommand2);
+		result2.execute();
+		display[1] = 0;
+		assertEquals(true, arrayEquals(RunLogic.getCurrentDisplay(), display));
+		
+		result1.undo();
 	}
 
 }
